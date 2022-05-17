c4d8ac7bbf381f61819dc65dc32129500cb23d2c
reemapdlhamed <93556835+reemapdlhamed@users.noreply.github.com>
2022-05-17 15:57:50 +0200

    edit

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -2,24 +2,22 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiService {
-  
   apiURL: string = 'http://projectegy-003-site13.gtempurl.com';
 
   constructor(private httpClient: HttpClient) {}
 
-  public getQuizById(id: number){
+  public getQuizById(id: number) {
     return this.httpClient.get(`${this.apiURL}/SolveQuiz/GetQustions?id=${id}`);
   }
 
-  public getResult(ansIds:number[]){
-    return this.httpClient.post(`${this.apiURL}/api/Quizzes/Solve`,{
-      "forQuizId":1,
-      "selectedAnswerIds": ansIds
+  public getResult(ansIds: number[]) {
+    // return this.httpClient.post(`${this.apiURL}/api/Quizzes/Solve`,{
+    return this.httpClient.post(`${this.apiURL}/SolveQuiz/SolveQuiz`, {
+      forQuizId: 1,
+      selectedAnswerIds: ansIds,
     });
   }
-
-
 }
